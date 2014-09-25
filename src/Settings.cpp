ba23a174e37d9ed4a6211a32568297212157531b
Ahn, SeungRye <seungrye@gmail.com>
2014-09-25 16:30:27 +0900

    fix compile error on android-ndk-r9

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -375,8 +375,8 @@ void Settings_Interpret( char option, const char *optarg, thread_Settings *mExtS
             break;
 
         case 'h': // print help and exit
-            fprintf(stderr, usage_long1);
-            fprintf(stderr, usage_long2);
+            fprintf(stderr, "%s", usage_long1);
+            fprintf(stderr, "%s", usage_long2);
             exit(1);
             break;
 
@@ -482,7 +482,7 @@ void Settings_Interpret( char option, const char *optarg, thread_Settings *mExtS
             break;
 
         case 'v': // print version and exit
-            fprintf( stderr, version );
+            fprintf( stderr, "%s", version );
             exit(1);
             break;
 
